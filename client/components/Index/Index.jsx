d2608b2616a408901e6cb6b25ec3175e93114ff5
Sagang Wee <sagangwee@yahoo.com>
2017-06-14 22:51:49 -0700

    blank highmap

--- client/components/Index/Index.jsx
+++ client/components/Index/Index.jsx
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
+import StateMap from '../StateMap';
 
 class IndexComponent extends Component {
   render() {
-    if (this.props.items.length === 0) {
-      return (
-        <p ref="empty">Index is empty.</p>
-      );
-    }
 
     return (
       <section>
         <h2 className="text-center opening-title">Visualizing Education</h2>
-        <ul ref="indexList" className="index-list">
-          {this.props.items.map((item, index) => {
-            return (<li key={index}>item {item}</li>);
-          })}
-        </ul>
+        <StateMap/>
       </section>
     );
   }
 }
-
-IndexComponent.defaultProps = {
-  items: []
-};
 
 export default IndexComponent;
--- client/components/StateMap.jsx
+++ client/components/StateMap.jsx
@@ -1,5 +1,82 @@
 import React, { Component } from 'react';
+import ReactHighmaps from 'react-highcharts/ReactHighmaps.src';
+import USMap from '../data/usmap';
+import data from '../data/state-math-reading-lunch.json';
 
-export default class StateMap extends React.Component {
+export default class StateMap extends Component {
+  render() {
+  	const config = {
+  	  chart: {
+        spacingBottom: 20
+    	},
 
+	    title: {
+	      text: 'Highmaps basic demo'
+	    },
+
+	    subtitle: {
+	      text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/us/us-all.js">United States of America</a>'
+	    },
+
+	    mapNavigation: {
+        enabled: true,
+        buttonOptions: {
+          verticalAlign: 'bottom'
+        }
+	    },
+
+	    colorAxis: {
+	      min: 0
+	    },
+
+	    plotOptions: {
+		    map: {
+		      allAreas: false,
+		      joinBy: ['iso-a2', 'code'],
+		      dataLabels: {
+		        enabled: true,
+		        color: 'white',
+		        style: {
+		          fontWeight: 'bold'
+		        }
+		      },
+		      mapData: USMap,
+		      tooltip: {
+		        headerFormat: '',
+		        pointFormat: '{point.name}: <b>{series.name}</b>'
+		      }
+
+		    }
+		  },
+
+	    series: [{
+        data: data,
+        joinBy: ['name', 0],
+        keys: ['name', '4math', '8math', '4read', '8read', 'lunch'],
+        name: 'Random data',
+        states: {
+          hover: {
+            color: '#BADA55'
+          }
+        },
+        dataLabels: {
+            enabled: true,
+            format: '{point.name}'
+        }
+	    }, {
+        name: 'Separators',
+        type: 'mapline',
+        color: 'silver',
+        showInLegend: false,
+        enableMouseTracking: false
+      }]
+	  }
+
+	  return (
+	  	<div>
+	  		<ReactHighmaps config={ config }/>
+	  	</div>
+	  );
+
+  }
 }
